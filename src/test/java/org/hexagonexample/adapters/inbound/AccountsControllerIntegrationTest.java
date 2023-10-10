86b1c043d03b9c69860ae3101ca0d0cdc9100572
mcklx <mcklex9@gmail.com>
2023-10-10 15:34:57 +0200

    tests: add get accounts tests

--- src/test/java/org/hexagonexample/adapters/inbound/AccountsControllerIntegrationTest.java
+++ src/test/java/org/hexagonexample/adapters/inbound/AccountsControllerIntegrationTest.java
@@ -1,7 +1,10 @@
 package org.hexagonexample.adapters.inbound;
 
 import org.hexagonexample.application.AccountsFacade;
+import org.hexagonexample.application.ports.inbound.AccountDto;
 import org.hexagonexample.application.ports.inbound.CreateAccountCommand;
+import org.hexagonexample.application.ports.inbound.GetAccountsCommand;
+import org.hexagonexample.domain.AccountId;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,10 +12,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AccountsController.class)
@@ -59,8 +67,18 @@ class AccountsControllerIntegrationTest {
 
     @Test
     public void shouldReturnAllAccounts() throws Exception {
+        AccountDto account1 = account(BigDecimal.ONE);
+        AccountDto account2 = account(BigDecimal.ZERO);
+        when(accountsFacade.handle(any(GetAccountsCommand.class))).thenReturn(List.of(account1, account2));
+
         mockMvc.perform(get("/accounts"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accounts", hasSize(2)))
+                .andExpect(jsonPath("$.accounts.[0].accountId.id", equalTo(account1.accountId().getId().toString())))
+                .andExpect(jsonPath("$.accounts.[1].accountId.id", equalTo(account2.accountId().getId().toString())));
     }
 
+    private AccountDto account(BigDecimal limit) {
+        return new AccountDto(AccountId.generate(), limit);
+    }
 }
